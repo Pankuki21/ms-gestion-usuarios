c3a01eee7d52970be7a9297f377bfd51394f157f
fra.acosta@duocuc.cl <fra.acosta@duocuc.cl>
2026-06-02 19:26:19 -0400

    Despliegue 1

---
+++ src/main/java/com/duoc/gestionusuarios/controller/HolaMundoController.java
@@ -0,0 +1,12 @@
+package com.duoc.gestionusuarios.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+    @GetMapping("/")
+    public String hola() {
+        return "Hola Mundo desde Spring Boot!";
+    }
+}
